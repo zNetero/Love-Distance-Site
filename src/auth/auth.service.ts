62b5794e531e992c88f9aa171acda1b874be3e3a
Giordano <giordanob056@gmail.com>
2025-10-02 01:23:30 +0200

    Updating signIn method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,17 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService} from '../users/users.service';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(private usersService: UsersService) {}
+    constructor(
+        private usersService: UsersService,
+        private jwtService: JwtService,
+    ) {}
 
-    async signIn(username: string, pass: string): Promise<any>{
-        const user = await this.usersService.findOne(username);
+    async signIn(email: string, pass: string): Promise<{ access_token: string}>{
+        const user = await this.usersService.findOne(email);
         if(!user){
             throw new UnauthorizedException('Invalid credentials');
         }
@@ -16,9 +20,10 @@ export class AuthService {
             throw new UnauthorizedException('Invalid credentials');
         }
 
-        const { password, ...result } = user;
-        //Generate and return JWT token
-        return result;
-        
+        const payload  = { sub: user.id, username: user.email };
+        return {
+            access_token: await this.jwtService.signAsync(payload),
+        }
+
     }
 }
