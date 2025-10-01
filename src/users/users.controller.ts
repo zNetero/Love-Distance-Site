4f677e77c2de08579557458dc246738eee43cd7d
Giordano <giordanob056@gmail.com>
2025-10-02 01:21:18 +0200

    Adding Post route

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body} from '@nestjs/common';
 import { UsersService } from './users.service';
+import { User } from './users.entity'
 //test initial route
 @Controller('users')
 export class UsersController {
-    constructor(private usersService: UsersService){}
+    constructor(private readonly usersService: UsersService){}
+
+    @Post()
+    create(@Body() user:User): Promise<User>{
+        return this.usersService.create(user);
+    }
 
     @Get()
     findAll(){
