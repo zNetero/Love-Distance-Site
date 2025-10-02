2282b16bd04f559f8be5413acfd5c2951d2212ac
Giordano <giordanob056@gmail.com>
2025-10-02 21:35:05 +0200

    Fixing Configmodule and Configservice

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,16 +2,19 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
-import { jwtConstants } from './constants';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 
 @Module({
   imports: [UsersModule,
-    JwtModule.register({
-      global: true,
-      secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {expiresIn: '60m'},
+      }),
+      inject: [ConfigService],
     })
   ],
   controllers: [AuthController],
