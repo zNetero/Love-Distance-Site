ad202f0c4413bd63a0913862075b261fe761f0f3
Giordano <giordanob056@gmail.com>
2025-10-04 06:36:31 +0200

    Adding CouplesModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { CouplesModule } from './couples/couples.module';
 //File to config database
 @Module({
   imports: [ConfigModule.forRoot({isGlobal: true,}),
@@ -20,7 +21,8 @@ import { ConfigModule } from '@nestjs/config';
     synchronize: true
   }),
   AuthModule,
-  UsersModule],
+  UsersModule,
+  CouplesModule],
   controllers: [AppController],
   providers: [AppService],
 })
